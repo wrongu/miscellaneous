6a52668351f7e0748b786ca630aba839d3c3b497
wrongu <lange.richard.d@gmail.com>
2013-12-20 16:38:49 -0500

    forgot to return the point before

--- cookie_cutter.py
+++ cookie_cutter.py
@@ -11,17 +11,17 @@ def create_cookie_cutter_box_mesh(img, width, height, thickness, depression, div
 	space_w = width / div_w
 	space_h = height / div_h
 	# initialize points and faces
-	pts = []
+	corners = []
 	faces = []
 	# add corner vertices
-	pts.append((0, 0, 0))
-	pts.append((w, 0, 0))
-	pts.append((w, h, 0))
-	pts.append((0, h, 0))
-	pts.append((0, 0, t))
-	pts.append((w, 0, t))
-	pts.append((w, h, t))
-	pts.append((0, h, t))
+	corners.append((0, 0, 0))
+	corners.append((w, 0, 0))
+	corners.append((w, h, 0))
+	corners.append((0, h, 0))
+	corners.append((0, 0, t))
+	corners.append((w, 0, t))
+	corners.append((w, h, t))
+	corners.append((0, h, t))
 	# bottom
 	faces.append((0,1,2))
 	faces.append((0,2,3))
@@ -37,13 +37,18 @@ def create_cookie_cutter_box_mesh(img, width, height, thickness, depression, div
 	# right
 	faces.append((2,5,1))
 	faces.append((2,6,5))
+
+	# map corner indices to corner locations
+	faces = [(corners[i], corners[j], corners[k]) for (i,j,k) in faces]
+
 	# create mesh of other points:
-	for xi in range(div_w + 1):
+	heightmap = {}
+	for xi in xrange(div_w + 1):
 		x = w * (float(xi) / float(div_w))
 		px = int(im_w * (float(xi) / float(div_w)))
 		if px >= im_w:
 			px = im_w-1
-		for yi in range(div_h + 1):
+		for yi in xrange(div_h + 1):
 			y = h * (float(yi) / float(div_h))
 			py = int(im_h * (float(yi) / float(div_h)))
 
@@ -56,24 +61,25 @@ def create_cookie_cutter_box_mesh(img, width, height, thickness, depression, div
 			if light > 128:
 				z = t - d
 
-			pts.append((x, y, z))
+			heightmap[(xi,yi)] = z
 
-	# add faces for all those points
+	def get_heightmapped_point(xi, yi):
+		x = w * (float(xi) / float(div_w))
+		y = h * (float(yi) / float(div_h))
+		z = heightmap[(xi, yi)]
+		return (x, y, z)
 
-	def grid_to_pt_number(grid_x, grid_y):
-		return 8 + div_h * grid_x + grid_y
-
-	for xi in range(div_w):
-		for yi in range(div_h):
-			bl = grid_to_pt_number(xi, yi)
-			tl = grid_to_pt_number(xi, yi+1)
-			br = grid_to_pt_number(xi+1, yi)
-			tr = grid_to_pt_number(xi+1, yi+1)
+	# add faces for all those points:
+	for xi in xrange(div_w):
+		for yi in xrange(div_h):
+			bl = get_heightmapped_point(xi, yi)
+			tl = get_heightmapped_point(xi, yi+1)
+			br = get_heightmapped_point(xi+1, yi)
+			tr = get_heightmapped_point(xi+1, yi+1)
 			# add 2 triangles
 			faces.append((bl, br, tr))
 			faces.append((tr, tl, bl))
-
-	return pts, faces
+	return faces
 
 if __name__ == '__main__':
 	if len(argv) > 1:
@@ -87,14 +93,12 @@ if __name__ == '__main__':
 		x_res = 300
 		y_res = 200
 		print "creating mesh"
-		pts, faces = create_cookie_cutter_box_mesh(img, width, height, thickness, sink, x_res, y_res)
-		print "mapping mesh"
-		mapped_faces = [(pts[i], pts[j], pts[k]) for i,j,k in faces]
+		faces = create_cookie_cutter_box_mesh(img, width, height, thickness, sink, x_res, y_res)
 
 		outfile = "output.stl" if len(argv) < 3 else argv[2]
 		print "ASCII writing to", outfile
 		from stl import Binary_STL_Writer, ASCII_STL_Writer
-		with open(outfile, "wb") as fp:
+		with open(outfile, "w") as fp:
 			writer = ASCII_STL_Writer(fp)
-			writer.add_faces(mapped_faces)
+			writer.add_faces(faces)
 			writer.close()
